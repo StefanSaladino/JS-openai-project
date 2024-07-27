b282770384f7e61755db54ae2477bc73bfa8d50c
StefanSaladino <200551988@student.georgianc.on.ca>
2024-07-27 10:30:45 -0400

    removed unnecessary routes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 // Load environment variables
 dotenv.config();
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
